d1f4dcc85f0a65ee0bbd9642da186dc2029ff458
caoxiaosheng <836900367@qq.com>
2017-12-14 00:57:08 +0800

    开始需求界面

---
+++ DemandManagementServer/Models/Demand.cs
@@ -0,0 +1,40 @@
+﻿namespace DemandManagementServer.Models
+{
+    public class Demand
+    {
+        public int Id { get; set; }
+
+        public DemandType DemandType { get; set; }
+
+        public string DemandDetail { get; set; }
+
+        public int UserId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public DemandPhase DemandPhase { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual Customer Customer { get; set; }
+    }
+
+    public enum DemandType
+    {
+        网管,
+        系统
+    }
+
+    public enum DemandPhase
+    {
+        需求提出,
+        需求分析,
+        版本计划,
+        功能开发,
+        等待反馈,
+        打回,
+        完成
+    }
+    
+
+}
